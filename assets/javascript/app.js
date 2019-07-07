76e07f3b94fd8cc051116104664f10fe3bba3ce8
JoJo <jae.kae.tea@gmail.com>
2019-07-07 16:59:35 -0500

    Got a working program

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,28 +18,32 @@ $("#addTrain").on("click", function(event) {
 
     var name = $("#name-input").val().trim();
     var destination = $("#destination-input").val().trim();
-    var first = $("#first-input").val().trim();
+    var first = moment($("#first-input").val().trim(), "HH:mm").format("X"); 
     var frequency = $("#rate-input").val().trim();
 
-    var newTrain = {
-        name: name,
-        destination: destination,
-        first: first,
-        frequency: frequency 
-    };
+    if ((name != "") && (destination != "") && (first >= 0) && (first <= 1562562000) && (frequency > 0)) {
+        var newTrain = {
+            name: name,
+            destination: destination,
+            first: first,
+            frequency: frequency 
+        };
 
-    database.ref().push(newTrain);
+        database.ref().push(newTrain);
 
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.first);
-    console.log(newTrain.frequency);
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.first);
+        console.log(newTrain.frequency);
 
-    $("#name-input").val("");
-    $("#destination-input").val("");
-    $("#first-input").val("");
-    $("#rate-input").val("");
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#first-input").val("");
+        $("#rate-input").val("");
 
+    } else {
+        alert("Invalid input; Please Try Again.");
+    }
 });
 
 database.ref().on("child_added", function(childSnapshot) {
@@ -55,11 +59,38 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log(first);
     console.log(frequency);
 
+    var f = first; 
+    var randomFormat = "X";
+    var convertedTime = moment(f, randomFormat);
+
+    var firstTimeConverted = convertedTime.format("HH:mm");
+    console.log("HEYYY " + firstTimeConverted);
+    firstTimeConverted = moment(convertedTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    
+    var currentTime = moment();
+    console.log("Current time: " + moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("Difference in time: " + diffTime);
+
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("Minutes till train: " + tMinutesTillTrain);
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm A");
+    console.log("Arrival Time: " + nextTrain);
+
+    //Name, Destination, Frequency, Next Arrival, minutes
     var newRow = $("<tr>").append(
         $("<td>").text(name),
         $("<td>").text(destination),
         $("<td>").text(frequency),
-        $("<td>").text(first),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain)
     );
 
     $("#trainTable > tbody").append(newRow);
